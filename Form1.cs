2835bb77d964ba143fd9768fb5712a964e227887
Amit Timalsina <amittimalsina999@gmail.com>
2024-12-07 09:56:01 +0545

    Removed bin/ and obj/ from version

---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double firstNumber = 0;   // To store the first number
+        double secondNumber = 0;  // To store the second number
+        string operation = "";    // To store the operator
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "9";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "0";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(txtDisplay.Text);
+            operation = "+";
+            txtDisplay.Clear();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(txtDisplay.Text);
+            operation = "-";
+            txtDisplay.Clear();
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(txtDisplay.Text);
+            operation = "*";
+            txtDisplay.Clear();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(txtDisplay.Text);
+            operation = "/";
+            txtDisplay.Clear();
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            secondNumber = Convert.ToDouble(txtDisplay.Text);
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber != 0)
+                    {
+                        result = firstNumber / secondNumber;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cannot divide by zero!");
+                    }
+                    break;
+            }
+
+            txtDisplay.Text = result.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            firstNumber = 0;
+            secondNumber = 0;
+            operation = "";
+        }
+    }
+}
